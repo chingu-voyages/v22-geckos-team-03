27a9b70cb92c1e056f39d38cd4827c7ec004ac05
GreatDeveloper66 <ashaffe@gmail.com>
2020-08-12 17:33:14 -0500

    Add mongoose connect statements

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const path = require('path')
-const MongoClient = require('mongodb').MongoClient
-const uri = 
-	`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}
-	@cluster0-vo8rx.mongodb.net/${process.env.MONGO_DBNAME}?retryWrites=true&w=majority`;
+const mongoose = require('mongoose')
+const uri = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0-vo8rx.mongodb.net/${process.env.MONGO_DBNAME}?retryWrites=true&w=majority`;
+console.log(uri)
+mongoose.Promise = Promise
+/*
+mongoose.connect(dbUrl, { useNewUrlParser: true}, err => {
+    console.log('mongo db connection', err)
+})
+*/
 
-
-
+mongoose.connect(uri)
+	.then(db => {
+		console.log(`Mongoose connection successful to host: ${db.connections[0].host} on port: ${db.connections[0].port}`)
+	})
+	.catch(dbErr => {
+		console.log('DB Connection Error:', dbErr.message)
+		process.exit(1)
+	})
 
 
 const PORT = process.env.PORT || 5000;
