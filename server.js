d375b2b8e24e64c89ecc1a41ad10d26836b01f9b
GreatDeveloper66 <ashaffe@gmail.com>
2020-08-12 16:57:55 -0500

    Add mongod connection to server.js

--- server.js
+++ server.js
@@ -1,21 +1,19 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const MongoClient = require('mongodb').MongoClient
+const uri = 
+	`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}
+	@cluster0-vo8rx.mongodb.net/${process.env.MONGO_DBNAME}?retryWrites=true&w=majority`;
+
+
+
 
 const PORT = process.env.PORT || 5000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-/*
-app.use(express.static(path.join(__dirname, "client","build")))
 
-app.get('*', (req,res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"))
-})
-*/
-
-
-// Bootstrap server
 app.listen(PORT, () => {
 	console.log(`Server listening on port ${PORT}.`);
 });
