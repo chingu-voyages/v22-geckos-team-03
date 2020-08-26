b46d0537ca4e5501f4b7c4fb0ec7c3cd6140416b
armchair-traveller <sentient.sober.armchair@gmail.com>
2020-08-25 17:11:41 -0700

    add login page

---
+++ frontend/src/routes/login.svelte
@@ -0,0 +1,9 @@
+<svelte:head>
+  <title>Login</title>
+</svelte:head>
+
+<!-- TODO: wire up form submit handler -->
+<form on:submit|preventDefault={(e) => ''}>
+  <input type="text" placeholder="user" />
+  <input type="password" placeholder="password" />
+</form>
