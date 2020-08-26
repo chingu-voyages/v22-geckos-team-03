efc7febfcc1a22145c2599c60afc8fb2438b0825
armchair-traveller <sentient.sober.armchair@gmail.com>
2020-08-25 17:22:05 -0700

    extend submit handler example

--- frontend/src/routes/login.svelte
+++ frontend/src/routes/login.svelte
@@ -1,3 +1,10 @@
+<script>
+  // TODO: wire up form submit handler
+  function loginSubmit(e) {
+    console.log(e.target[0].value, e.target[0].value);
+  }
+</script>
+
 <style>
   input {
     min-height: 2rem;
@@ -10,7 +17,8 @@
 </svelte:head>
 
 <!-- TODO: wire up form submit handler -->
-<form on:submit|preventDefault={(e) => ''}>
+<form on:submit|preventDefault={loginSubmit}>
   <input type="text" placeholder="user" />
   <input type="password" placeholder="password" />
+  <button type="submit">Login</button>
 </form>
