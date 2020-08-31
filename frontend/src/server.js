722017a44fd931f2a5be764b75c1cecdd9ed1b25
GreatDeveloper66 <ashaffe@gmail.com>
2020-08-31 18:01:10 -0500

    Add body parser imports

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -3,6 +3,7 @@ import polka from 'polka';
 import compression from 'compression';
 import * as sapper from '@sapper/server';
 import mongodb from 'mongodb';
+import bodyParser from 'body-parser'
 
 
 const MongoClient = mongodb.MongoClient
@@ -21,6 +22,8 @@ const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
 
 polka() // You can also use Express
+	.use(bodyParser.urlencoded({ extended: true }))
+	.use(bodyParser.json())
 	.get('/getit',(req,res)=> {
 		res.end('hello')
 	})
