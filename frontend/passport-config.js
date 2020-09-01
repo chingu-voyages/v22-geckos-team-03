4e0ada372e8ad761deaa6e708a5c746690b238b2
GreatDeveloper66 <ashaffe@gmail.com>
2020-09-01 11:05:04 -0500

    make authenticate user function asynchronous

--- frontend/passport-config.js
+++ frontend/passport-config.js
@@ -2,7 +2,7 @@ const LocalStrategy = require('passport-local').Strategy
 const bcrypt = require('bcrypt')
 
 function initialize(passport) {
-    const authenticateUser = (email, password, done) => {
+    const authenticateUser = async (email, password, done) => {
         const user = getUserByEmail(email)
         if (user === null){
             return done(null, false, {message: 'No user with that email'})
