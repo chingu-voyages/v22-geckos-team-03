fa61a606e24f1910ebe1bcecdc0b7b82be2905a2
GreatDeveloper66 <ashaffe@gmail.com>
2020-09-01 09:21:21 -0500

    Add passport-config function

--- frontend/passport-config.js
+++ frontend/passport-config.js
@@ -1,5 +1,28 @@
-function initialize() {
+const LocalStrategy = require('passport-local').Strategy
+const bcrypt = require('bcrypt')
 
+function initialize(passport) {
+    const authenticateUser = (email, password, done) => {
+        const user = getUserByEmail(email)
+        if (user === null){
+            return done(null, false, {message: 'No user with that email'})
+        }
+        try {
+            if(await bcrypt.compare(password, user.password)){
+                return done(null, user)
+            }
+            else {
+                return done(null, false, {message: 'Password incorrect'})
+            }
+        }
+        catch(e) {
+            return done(e)
+        }
+    }
+    passport.use(new LocalStrategy({ usernameField: 'email' }),
+        authenticateUser)
+    passport.serializeUser((user, done) => { })
+    passport.deserializeUser((id, done) => { })
 }
 
 export default initialize
