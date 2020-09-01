adc98ccd22ca7dc1c3f2f137d2d94f1a5b27fc2c
GreatDeveloper66 <ashaffe@gmail.com>
2020-09-01 08:08:34 -0500

    Add bcrypt hash password to post route

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -4,8 +4,9 @@ import compression from 'compression';
 import * as sapper from '@sapper/server';
 import mongodb from 'mongodb';
 import bodyParser from 'body-parser'
+const bcrypt = require('bcrypt')
 
-
+/*
 const MongoClient = mongodb.MongoClient
 const mongoURL = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017'
 const dbName = 'chat-app'
@@ -17,6 +18,7 @@ MongoClient.connect(mongoURL, { useNewUrlParser: true }, (err, client) => {
     console.log(`Connected MongoDB: ${mongoURL}`)
     console.log(`Database: ${dbName}`)
 })
+*/
 
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
@@ -27,9 +29,55 @@ polka() // You can also use Express
 	.get('/getit',(req,res)=> {
 		res.end('hello')
 	})
-	.post('/getit',(req,res) => {
-		console.log(req.body)
-		res.end(JSON.stringify(req.body))
+	.post('/registerUser', async (req,res) => {
+		try {
+			const hashedPassword = await bcrypt.hash(req.body.password, 10)
+			
+				const userObj = {
+					id: Date.now().toString(),
+					userName: req.body.userName,
+					email: req.body.email,
+					password: hashedPassword
+				}
+				
+				mongodb.MongoClient.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017',
+											{ useNewUrlParser: true }, (err, client) => { 
+												if(err) return console.log(err)
+												client.db('chat-app').collection('User').insertOne(userObj)
+					//db = client.db(dbName)
+					//console.log(`Connected MongoDB: ${mongoURL}`)
+					//console.log(`Database: ${dbName}`)
+					//db.collection('User').insert(userObj)
+											})
+											
+			res.end('user registered')
+		}
+		catch {
+			res.end('error')
+		}
+		/*
+		.post('/registerUser', async (req,res) => {
+			try {
+
+				
+				const MongoClient = mongodb.MongoClient
+				const mongoURL = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017'
+				const dbName = 'chat-app'
+				let db = null
+				MongoClient.connect(mongoURL, { useNewUrlParser: true }, (err, client) => {
+					if(err) return console.log(err)
+					db = client.db(dbName)
+					console.log(`Connected MongoDB: ${mongoURL}`)
+					console.log(`Database: ${dbName}`)
+					db.collection('User').insert(userObj)
+				})
+			} catch {
+				res.end('error')
+			}
+			console.log(req.body)
+			res.end(JSON.stringify(req.body))
+		})
+		*/
 	})
 	.use(
 		compression({ threshold: 0 }),
