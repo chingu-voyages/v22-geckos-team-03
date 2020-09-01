68bf113951b9a05bea40a83ff72ccdbfe819c590
GreatDeveloper66 <ashaffe@gmail.com>
2020-09-01 13:40:26 -0500

    Add getUserByEmail and getUserById to initialize

--- frontend/passport-config.js
+++ frontend/passport-config.js
@@ -1,7 +1,7 @@
 const LocalStrategy = require('passport-local').Strategy
 const bcrypt = require('bcrypt')
 
-function initialize(passport) {
+function initialize(passport, getUserByEmail, getUserById) {
     const authenticateUser = async (email, password, done) => {
         const user = getUserByEmail(email)
         if (user === null){
@@ -20,8 +20,8 @@ function initialize(passport) {
         }
     }
     passport.use(new LocalStrategy({ usernameField: 'email' }, authenticateUser))
-    passport.serializeUser((user, done) => { })
-    passport.deserializeUser((id, done) => { })
+    passport.serializeUser((user, done) => done(null, user.id))
+    passport.deserializeUser((id, done) => done(null, getUserById(id)))
 }
 
 export default initialize
